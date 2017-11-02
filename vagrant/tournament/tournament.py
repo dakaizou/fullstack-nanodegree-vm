660716f598e05ae9d3d5243a072aae288edb8f39
dakaizou <dakaizou@gmail.com>
2017-11-02 10:17:46 +0800

    style fixed

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 
 database = 'dbname=tournament'
+
+
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
@@ -35,10 +37,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -50,8 +52,8 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -75,18 +77,18 @@ def reportMatch(winner, loser):
     """
     with psycopg2.connect(database) as conn:
         with conn.cursor() as c:
-            # c.execute('INSERT INTO matches (player1_id, player2_id, winner_id) SELECT x.id, y.id, x.id FROM players AS x, players AS y WHERE x.name = %s AND y.name = %s', (winner, loser))
-            c.execute('INSERT INTO matches (winner_id, loser_id) VALUES (%s, %s)', (winner, loser))
- 
- 
+            query = 'INSERT INTO matches (winner_id, loser_id) VALUES (%s, %s)'
+            c.execute(query, (winner, loser))
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -105,6 +107,3 @@ def swissPairings():
             match = []
             continue
     return pairs
-
-
-
